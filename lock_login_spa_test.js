b3ef56eccf03fbfd1a67ce8bf4d5dd3c891976a8
Steve Hobbs <elkdanger@gmail.com>
2019-06-05 23:15:59 +0100

    Bumped time on all UI wait commands

--- lock_login_spa_test.js
+++ lock_login_spa_test.js
@@ -2,14 +2,14 @@ Feature("SPA login with new responsive design");
 
 Scenario("Log in using Lock", I => {
   I.amOnPage("/");
-  I.waitForVisible("#qsLoginBtn", 3);
+  I.waitForVisible("#qsLoginBtn", 10);
   I.click("#qsLoginBtn");
-  I.waitForVisible('input[name="password"]', 5);
+  I.waitForVisible('input[name="password"]', 10);
   I.wait(1);
   I.fillField('input[name="username"]', "asdasd");
   I.fillField('input[name="password"]', "asdasd");
   I.click(".auth0-lock-widget-container .auth0-lock-submit");
-  I.waitForVisible("#profileDropDown", 5);
+  I.waitForVisible("#profileDropDown", 10);
   I.click("#profileDropDown");
   I.waitForVisible("#qsLogoutBtn");
   I.click("#qsLogoutBtn");
