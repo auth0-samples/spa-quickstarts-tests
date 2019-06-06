a6c509585242523a681f96f580543d445919a484
Steve Hobbs <elkdanger@gmail.com>
2019-06-06 13:15:30 +0100

    Bumped timeout values of SPA tests (#2)
    * Bumped the time to wait for login button to appear
    
    * Bumped time on all UI wait commands

--- lock_login_spa_test.js
+++ lock_login_spa_test.js
@@ -1,18 +1,18 @@
-Feature('SPA login with new responsive design');
+Feature("SPA login with new responsive design");
 
-Scenario('Log in using Lock', (I) => {
-    I.amOnPage('/');
-    I.waitForVisible('#qsLoginBtn', 3);
-    I.click('#qsLoginBtn');
-    I.waitForVisible('input[name="password"]', 5);
-    I.wait(1);
-    I.fillField('input[name="username"]', 'asdasd');
-    I.fillField('input[name="password"]', 'asdasd');
-    I.click('.auth0-lock-widget-container .auth0-lock-submit');
-    I.waitForVisible('#profileDropDown', 5);
-    I.click('#profileDropDown');
-    I.waitForVisible('#qsLogoutBtn');
-    I.click('#qsLogoutBtn')
-    I.waitForVisible('#qsLoginBtn', 5);
-    I.amOnPage('/');
+Scenario("Log in using Lock", I => {
+  I.amOnPage("/");
+  I.waitForVisible("#qsLoginBtn", 10);
+  I.click("#qsLoginBtn");
+  I.waitForVisible('input[name="password"]', 10);
+  I.wait(1);
+  I.fillField('input[name="username"]', "asdasd");
+  I.fillField('input[name="password"]', "asdasd");
+  I.click(".auth0-lock-widget-container .auth0-lock-submit");
+  I.waitForVisible("#profileDropDown", 10);
+  I.click("#profileDropDown");
+  I.waitForVisible("#qsLogoutBtn");
+  I.click("#qsLogoutBtn");
+  I.waitForVisible("#qsLoginBtn", 10);
+  I.amOnPage("/");
 });
