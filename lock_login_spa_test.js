c6b530633ea3761044f78dca2b569166e841d3da
Steve Hobbs <elkdanger@gmail.com>
2019-06-19 10:28:54 +0100

    Bumped timings to fix timeout issues with slow rendering (#3)

--- lock_login_spa_test.js
+++ lock_login_spa_test.js
@@ -2,17 +2,17 @@ Feature("SPA login with new responsive design");
 
 Scenario("Log in using Lock", I => {
   I.amOnPage("/");
-  I.waitForVisible("#qsLoginBtn", 10);
+  I.waitForVisible("#qsLoginBtn", 20);
   I.click("#qsLoginBtn");
-  I.waitForVisible('input[name="password"]', 10);
+  I.waitForVisible('input[name="password"]', 20);
   I.wait(1);
   I.fillField('input[name="username"]', "asdasd");
   I.fillField('input[name="password"]', "asdasd");
   I.click(".auth0-lock-widget-container .auth0-lock-submit");
-  I.waitForVisible("#profileDropDown", 10);
+  I.waitForVisible("#profileDropDown", 20);
   I.click("#profileDropDown");
   I.waitForVisible("#qsLogoutBtn");
   I.click("#qsLogoutBtn");
-  I.waitForVisible("#qsLoginBtn", 10);
+  I.waitForVisible("#qsLoginBtn", 20);
   I.amOnPage("/");
 });
